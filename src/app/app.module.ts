ab9448ea430749a69e7574bf13d0c0ccc08003ef
Rbalyk <rbalik341@gmail.com>
2019-08-11 00:01:36 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { CdkTreeModule } from '@angular/cdk/tree';
+
+import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
+import {MatButtonModule, MatCardModule, MatGridListModule, MatListModule} from '@angular/material';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    DishdetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    CdkTreeModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
